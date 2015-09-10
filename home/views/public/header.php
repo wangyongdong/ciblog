0475718de7a0f8b9acb90c2973c38d4ca3f7cacd
wangyongdong-work <wangyongdong-work>
2015-09-10 15:36:22 +0800

    删除无用部分

--- home/views/public/header.php
+++ home/views/public/header.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html crossriderapp2258="true" class=" js flexbox canvas canvastext webgl no-touch geolocation postmessage no-websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients no-cssreflections csstransforms csstransforms3d csstransitions fontface video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths" dir="ltr" lang="en-US">
+<html lang="en-US">
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="Cache-Control" content="no-transform" />
 		<meta http-equiv="Cache-Control" content="no-siteapp" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="robots" content="noarchive" />
 		<meta name="keywords" content="<?=$keywords?>" />
 		<meta name="description" content="<?=$description?>" />
@@ -22,7 +22,7 @@
 			$(document).ready(function() {
 				$().UItoTop({ easingType: 'easeOutQuart' });
 			});
-			var __A = "<?php echo base_url(); ?>";	//定义_A的目的是在js文件中使用
+			var __A = "<?php echo base_url(); ?>";
 		</script>
 	</head>
 	<body class="home blog">
@@ -48,7 +48,7 @@
 							<li <?php if($header == 'about') {echo 'class="current"';}?>><a href="<?=site_url('about')?>">关于我</a></li>
 						</ul>
 					</div>
-				</div><!-- #access -->
+				</div>
 				<form id="navsearchform" role="search" method="get" action="<?=site_url("article")?>">
 					<input placeholder="文章快速搜索" value="<?=sg($aFilter['q'])?>" name="q" id="navs" type="text">
 					<input id="navsearchsubmit" value="" type="submit">
